70df397864621bd77c916c72a316eef4934be09a
Javier <javii_lb@hotmail.com>
2023-09-06 20:17:55 -0300

    dom

--- carritodom.js
+++ carritodom.js
@@ -1,79 +1,77 @@
-/*Con setItem enviamos la información al Storage.
+
 
 localStorage.setItem('bienvenida', '¡Hola Coder!');
 localStorage.setItem('esValido', true);
 localStorage.setItem('unNumero', 20);
 
-//Sacamos la información del Storage y la mostramos o le renderizamos en la pagina.
+
 let mensaje =  localStorage.getItem('bienvenida');
 let bandera =  localStorage.getItem('esValido');
 let numero  =  localStorage.getItem('unNumero');
 
-console.log(mensaje); // ‘¡Hola Coder!’
-console.log(bandera); // ‘true’
-console.log(numero);  // ‘20’ */
+console.log(mensaje); 
+console.log(bandera); 
+console.log(numero);  
 
-/* sessionStorage.setItem('seleccionados', [1,2,3]);
+ sessionStorage.setItem('seleccionados', [1,2,3]);
 sessionStorage.setItem('esValido', false);
-sessionStorage.setItem('email', 'info@email.com'); */
+sessionStorage.setItem('email', 'info@email.com'); 
 
-/* let lista   =  sessionStorage.getItem('seleccionados').split(",");
-let bandera = (sessionStorage.getItem('esValido') == 'true');
+let lista   =  sessionStorage.getItem('seleccionados').split(",");
+let banderaa = (sessionStorage.getItem('esValido') == 'true');
 let email   =  sessionStorage.getItem('email');
 
-console.log(typeof lista);   //object ["1","2","3"];
-console.log(typeof bandera); //boolean;
-console.log(typeof email);   //string; */
+console.log(typeof lista);   
+console.log(typeof bandera); 
+console.log(typeof email);   
 
-/* for (let i = 0; i < localStorage.length; i++) {
+for (let i = 0; i < localStorage.length; i++) {
     let clave = localStorage.key(i);
     console.log("Clave: "+ clave);
     console.log("Valor: "+ localStorage.getItem(clave));
 }
- */
+ 
 
-
-
-/* localStorage.setItem('bienvenida', '¡Hola Coder!');
+ localStorage.setItem('bienvenida', '¡Hola Coder!');
 localStorage.setItem('esValido', true);
- */
-//localStorage.removeItem('bienvenida');
-//localStorage.clear()
+ 
+localStorage.removeItem('bienvenida');
+localStorage.clear()
 
-/* const producto1 = { id: 2, producto: "Arroz" };
+const producto = { id: 2, producto: "Arroz" };
 const enJSON    = JSON.stringify(producto1);
 
-console.log(enJSON); // {"id":2,"producto":"Arroz"}
-console.log(typeof producto1); // object
-console.log(typeof enJSON);    // string
+console.log(enJSON); 
+console.log(typeof producto1);
+console.log(typeof enJSON);    
 
-localStorage.setItem("pepe", enJSON); */
+localStorage.setItem("pepe", enJSON); 
 
-/* const enJSON    = '{"id":2,"producto":"Arroz"}';
+const enJSON1    = '{"id":2,"producto":"computadora"}';
 const producto1 = JSON.parse(enJSON);
 
-console.log(typeof enJSON);     // string
-console.log(typeof producto1);  // object
-console.log(producto1.producto); // Arroz
+console.log(typeof enJSON);     
+console.log(typeof producto1);  
+console.log(producto1.producto); 
 
 const producto2 = JSON.parse(localStorage.getItem("producto1"));
 console.log(producto2.id);  // 2    */ 
 
-/* const productos = [{ id: 1,  producto: "Arroz", precio: 125 },
-                  {  id: 2,  producto: "Fideo", precio: 70 },
-                  {  id: 3,  producto: "Pan"  , precio: 50},
-                  {  id: 4,  producto: "Flan" , precio: 100}];
+const productos = [{ id: 1,  producto: "computadora", precio: 125000 },
+                  {  id: 2,  producto: "notebook", precio: 700000 },
+                  {  id: 3,  producto: "Pendrive"  , precio: 5000},
+                  {  id: 4,  producto: "monitor" , precio: 100000}];
 
 const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
-//Almacenar producto por producto
+
 for (const producto of productos) {
     guardarLocal(producto.id, JSON.stringify(producto));
 }
-// o almacenar array completo
-guardarLocal("listaProductos", JSON.stringify(productos));
- */
 
-/* class Producto {
+guardarLocal("listaProductos", JSON.stringify(productos));
+ 
+
+class Producto {
     constructor(obj) {
         this.nombre  = obj.producto.toUpperCase();
         this.precio  = parseFloat(obj.precio);
@@ -82,27 +80,24 @@ guardarLocal("listaProductos", JSON.stringify(productos));
         this.precio = this.precio * 1.21;
     }
 }
-//Obtenemos el listado de productos almacenado
+
 const almacenados = JSON.parse(localStorage.getItem("listaProductos"));
-const productos = [];
-//Iteramos almacenados con for...of para transformar todos sus objetos a tipo producto.
+const productos1 = [];
+
 for (const objeto of almacenados)
     productos.push(new Producto(objeto));
-//Ahora tenemos objetos productos y podemos usar sus métodos
+
 for (const producto of productos)
     producto.sumaIva();
- */
+ 
+let usuario;
+let usuarioEnLS = localStorage.getItem("usuario");
 
-
-    let usuario;
-    let usuarioEnLS = JSON.stringify(localStorage.getItem("usuario"))
-    
-    
-    // Si había algo almacenado, lo recupero. Si no le pido un ingreso
-    if (usuarioEnLS) {
-        usuario = usuarioEnLS
-    } else {
-        usuario = prompt("Ingrese su nombre de usuario")
+if (usuarioEnLS) 
+    { usuario = JSON.parse(usuarioEnLS);
+    } else 
+    {
+        usuario = prompt("Ingrese su nombre de usuario");
+        localStorage.setItem("usuario", JSON.stringify(usuario));
     }
-      
-
+    
